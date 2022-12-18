1470187a409b7e17311607939b17d1736c7ba35f
Ak <akshithgray17@gmail.com>
2022-12-18 18:55:01 +0530

    plaincss-setup

---
+++ src/Pages/Profile-View/ProfileView.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './ProfileView.css'
+
+const ProfileView = () => {
+  return (
+    <>
+    <div className='view-user-profile'>
+    <div className='user'>
+    <div className='user-container'>
+    <div className="user-profile-container">
+    </div>
+    <div className='view-username'>
+    <h1 >Username#1234</h1>
+    </div>
+        <div className='edit-profile-button'>
+        <button> Edit Profile</button>
+        </div>
+    </div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default ProfileView
