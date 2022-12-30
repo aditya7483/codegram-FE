d012b7c89ef017e1f716e740d4d1dfbbf1ee552b
Ak <akshithgray17@gmail.com>
2022-12-30 15:03:16 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+import "./App.css";
+// import { useState } from "react";
+// import Sidebarprofile from "./Pages/Profile-View/Sidebarprofile";
 import ProfileView from "./Pages/Profile-View/ProfileView";
+// import ProfileSetting from "./Pages/Profile-View/ProfileSetting";
+// import { BrowserRouter, Route,Navigate, Routes } from "react-router-dom";
+// import Box from "@mui/material/Box";
 function App() {
+  // const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   return (
     <>
-    <ProfileView/>
+    {/* <BrowserRouter>
+    <Box width="100%" height="100%">
+      <Sidebarprofile
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+        drawerWidth="250px"
+        user={"abc123"}    
+      />
+      </Box>
+      <Routes>
+        <Route path="/" element={<Navigate to="/ProfileView" replace />} />
+        <Route path="/ProfileView" element={<ProfileView/>}/>
+        <Route path="/ProfileSetting" element={<ProfileSetting />} />
+      </Routes>
+    </BrowserRouter> */}
+    
+      <ProfileView/>
     </>
   );
 }
--- src/Pages/Profile-View/ProfileView.js
+++ src/Pages/Profile-View/ProfileView.js
@@ -1,46 +1,56 @@
 import React from "react";
 import styles from "./ProfileView.module.css";
-import PropTypes from "prop-types";
+import { styled } from "@mui/material/styles";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import banner from "../../Assets/84f5533218ae3fbc050349d5d1937d13.jpg";
+// import banner from "../../Assets/84f5533218ae3fbc050349d5d1937d13.jpg";
 import profile from "../../Assets/identicon.png";
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+const AntTabs = styled(Tabs)({
+  borderBottom: "1px solid #e8e8e8",
+  "& .MuiTabs-indicator": {
+    backgroundColor: " #8400fd",
+  },
+});
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
+const AntTab = styled((props) => <Tab disableRipple {...props} />)(
+  ({ theme }) => ({
+    textTransform: "none",
+    minWidth: 0,
+    [theme.breakpoints.up("sm")]: {
+      minWidth: 0,
+    },
+    fontWeight: theme.typography.fontWeightRegular,
+    marginRight: theme.spacing(1),
+    color: "rgba(0, 0, 0, 0.85)",
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+    "&:hover": {
+      color: "#8400fd",
+      opacity: 1,
+    },
+    "&.Mui-selected": {
+      color: "#8400fd",
+      fontWeight: theme.typography.fontWeightMedium,
+    },
+    "&.Mui-focusVisible": {
+      backgroundColor: "#d1eaff",
+    },
+  })
+);
 const ProfileView = () => {
   const [value, setValue] = React.useState(0);
 
@@ -50,7 +60,7 @@ const ProfileView = () => {
 
   return (
     <>
-      <div
+      {/* <div
         className={styles.user_profile_banner}
         style={{
           backgroundImage: `url(${banner})`,
@@ -58,7 +68,8 @@ const ProfileView = () => {
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
-      >
+      ></div> */}
+      <div className={styles.user_page}>
         <div className={styles.user_profile_container}>
           <div
             className={styles.user_profile_pic}
@@ -68,79 +79,58 @@ const ProfileView = () => {
               backgroundSize: "cover",
             }}
           ></div>
-          <div className={styles.view_username}>
-            <h3>Username#1234</h3>
-            <h5>location</h5>
+
+          <div className={styles.view_userinfo}>
+            <h5>Username#1234</h5>
           </div>
+          <button type="button" className="btn btn-outline-primary" style={{width:"100%"}}>Set Profile</button>
         </div>
-      </div>
-      <div className={styles.div2}>
-        <div
-          className={styles.user_container}
-          style={{ justifyContent: "center" }}
-        >
-          <div className={styles.user}>
-            <Card sx={{ minWidth: 275, square: true }}>
+        <div className={` ${styles.user_container}`}>
+          <div className={styles.user_data}>
+            <Card sx={{ minWidth: 100, square: true }}>
               <CardContent>
-                <Typography
-                  sx={{ fontSize: 14 }}
-                  color="text.secondary"
-                  gutterBottom
-                >
+                <Typography sx={{ fontSize: 14 }} color="#8400fd" gutterBottom>
                   About Me
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  -----
-                </Typography>
-                <Typography variant="body2">
-                  -----.
-                  <br />
-                  {'"------"'}
                 </Typography>
               </CardContent>
             </Card>
-            <Box sx={{ width: "100%" }}>
-              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-                <Tabs
-                  value={value}
-                  onChange={handleChange}
-                  aria-label="basic tabs example"
-                >
-                  <Tab label="Overview" {...a11yProps(0)} />
-                  <Tab label="Social" {...a11yProps(1)} />
-                  <Tab label="Activity" {...a11yProps(2)} />
-                </Tabs>
-              </Box>
-              <TabPanel value={value} index={0}>
-                Overview
-              </TabPanel>
-              <TabPanel value={value} index={1}>
-                Social
-              </TabPanel>
-              <TabPanel value={value} index={2}>
-                Activity
-              </TabPanel>
-            </Box>
-          </div>
-          <Card sx={{ minWidth: 175, m: 2, square: true }}>
+            <Card sx={{ Width: 100, mt: 2, square: true, height: 80 }}>
             <CardContent>
-              <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
-                gutterBottom
-              >
-                Skills/contributors/users
+              <Typography sx={{ fontSize: 14 }} color="#8400fd" gutterBottom>
+                Skills
               </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                ----------
-              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary"></Typography>
               <Typography variant="body2">
-                ----------
                 <br />
-                {'"------------"'}
               </Typography>
             </CardContent>
           </Card>
+          
+          <Card sx={{ Width: 100, mt:2, square: true, height: 100 }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="#8400fd" gutterBottom>
+                Teams
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary"></Typography>
+              <Typography variant="body2">
+                <br />
+              </Typography>
+            </CardContent>
+          </Card>
+      
+            <Box sx={{ bgcolor: "#fff", mt: "1rem" }}>
+              <AntTabs
+                value={value}
+                onChange={handleChange}
+                aria-label="ant example"
+              >
+                <AntTab label="Overview" />
+                <AntTab value="Activity" label="Activity" />
+                <AntTab value="Social" label="Social" />
+              </AntTabs>
+              <Box sx={{ p: 3 }} />
+            </Box>
+          </div>
         </div>
       </div>
     </>
